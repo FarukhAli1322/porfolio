7164be2ae9190badd2294aad0a2213c513b6dbcd
AMIT KUMAR <97108600+amitkumar2308@users.noreply.github.com>
2025-12-03 16:38:02 +0530

    update ui

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -190,7 +190,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("9d1c95e1ccf71b44")
+/******/ 		__webpack_require__.h = () => ("5d9115d07dab96a1")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
